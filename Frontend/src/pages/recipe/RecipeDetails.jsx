16b91822c19538e2a85da4f9a8f7035a52fdb335
Matija1988 <matijapavkovic74@gmail.com>
2024-09-11 14:21:00 +0200

    no message

--- Frontend/src/pages/recipe/RecipeDetails.jsx
+++ Frontend/src/pages/recipe/RecipeDetails.jsx
@@ -1,10 +1,48 @@
+import { useEffect, useState } from "react";
 import { Container, Form } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
+import RecipeService from "../../services/RecipeService";
 
 export default function RecipeDetails() {
+  const recipeState = {
+    id: "",
+    title: "",
+    subtitle: "",
+    text: "",
+    categoryName: "",
+    members: [],
+  };
+  const [recipe, setRecipe] = useState(recipeState);
+  const [members, setMembers] = useState([]);
+  const [category, setCategory] = useState("");
+
+  const [error, setError] = useState([]);
+  const routeParams = useParams();
+  const navigate = useNavigate();
+
+  async function fetchRecipe() {
+    try {
+      const response = await RecipeService.getById("recipe", routeParams.id);
+      if (response.ok) {
+        setRecipe(response.data);
+        setMembers(response.data.members);
+        setCategory(response.data.categoryName);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchRecipe();
+  }, []);
+
   return (
     <>
       <Container className="primaryContainer">
-        <h1>PREVIEW</h1>
+        <h2>{recipe.title}</h2>
+        <h4>{recipe.subtitle}</h4>
+        <div dangerouslySetInnerHTML={{ __html: recipe.text }}></div>
       </Container>
     </>
   );
---
+++ Frontend/src/services/PictureService.js
@@ -0,0 +1,17 @@
+import {
+  httpService,
+  handleSuccess,
+  readAll,
+  getById,
+  create,
+  update,
+  setNotActive,
+} from "./HttpService";
+
+export default {
+  readAll,
+  create,
+  getById,
+  update,
+  setNotActive,
+};
