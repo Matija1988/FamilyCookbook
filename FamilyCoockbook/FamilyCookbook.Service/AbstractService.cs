31a4694c678053428fa62365a1d5b81136932c73
Matija1988 <matijapavkovic74@gmail.com>
2024-09-27 18:04:29 +0200

    AbstractService

---
+++ FamilyCoockbook/FamilyCookbook.Service/AbstractService.cs
@@ -0,0 +1,45 @@
+﻿using FamilyCookbook.Model;
+using FamilyCookbook.Respository.Common;
+using FamilyCookbook.Service.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyCookbook.Service
+{
+    public abstract class AbstractService<T> : IService<T> where T : class
+    {
+        protected readonly IRepository<T> _repository;
+
+        protected AbstractService(IRepository<T> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<RepositoryResponse<T>> DeleteAsync(int id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+
+        public async Task<RepositoryResponse<List<T>>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<RepositoryResponse<T>> GetByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task<RepositoryResponse<T>> SoftDeleteAsync(int id)
+        {
+            return await _repository.SoftDeleteAsync(id);
+        }
+
+        public async Task<RepositoryResponse<T>> UpdateAsync(int id, T entity)
+        {
+            return await _repository.UpdateAsync(id, entity);
+        }
+    }
+}
--- FamilyCoockbook/FamilyCookbook.Service/CommentService.cs
+++ FamilyCoockbook/FamilyCookbook.Service/CommentService.cs
@@ -9,9 +9,14 @@ using System.Threading.Tasks;
 
 namespace FamilyCookbook.Service
 {
-    public sealed class CommentService(ICommentRepository commentRepository) : ICommentService
+    public sealed class CommentService : AbstractService<Comment>, ICommentService
     {
-        private readonly ICommentRepository _commentRepository = commentRepository;
+        private  readonly ICommentRepository _commentRepository;
+
+        public CommentService(ICommentRepository commentRepository) : base(commentRepository) 
+        {
+            _commentRepository = commentRepository;
+        }
 
         public async Task<RepositoryResponse<Comment>> CreateAsync(Comment comment)
         {
@@ -24,41 +29,41 @@ namespace FamilyCookbook.Service
             return response;
         }
 
-        public async Task<RepositoryResponse<Comment>> DeleteAsync(int id)
-        {
-            var response = await _commentRepository.DeleteAsync(id);
+        //public async Task<RepositoryResponse<Comment>> DeleteAsync(int id)
+        //{
+        //    var response = await _commentRepository.DeleteAsync(id);
 
-            return response;
-        }
+        //    return response;
+        //}
 
-        public async Task<RepositoryResponse<List<Comment>>> GetAllAsync()
-        {
-            var response = await _commentRepository.GetAllAsync();
+        //public async Task<RepositoryResponse<List<Comment>>> GetAllAsync()
+        //{
+        //    var response = await _commentRepository.GetAllAsync();
 
-            return response;
-        }
+        //    return response;
+        //}
 
-        public async Task<RepositoryResponse<Comment>> GetByIdAsync(int id)
-        {
-            var response = await _commentRepository.GetByIdAsync(id);
+        //public async Task<RepositoryResponse<Comment>> GetByIdAsync(int id)
+        //{
+        //    var response = await _commentRepository.GetByIdAsync(id);
 
-            return response;
-        }
+        //    return response;
+        //}
 
-        public async Task<RepositoryResponse<Comment>> SoftDeleteAsync(int id)
-        {
-            var response = await _commentRepository.SoftDeleteAsync(id);
+        //public async Task<RepositoryResponse<Comment>> SoftDeleteAsync(int id)
+        //{
+        //    var response = await _commentRepository.SoftDeleteAsync(id);
 
-            return response;
-        }
+        //    return response;
+        //}
 
-        public async Task<RepositoryResponse<Comment>> UpdateAsync(int id, Comment entity)
-        {
-            entity.DateUpdated = DateTime.Now;
+        //public async Task<RepositoryResponse<Comment>> UpdateAsync(int id, Comment entity)
+        //{
+        //    entity.DateUpdated = DateTime.Now;
 
-            var resposne = await _commentRepository.UpdateAsync(id, entity);
+        //    var resposne = await _commentRepository.UpdateAsync(id, entity);
 
-            return resposne;
-        }
+        //    return resposne;
+        //}
     }
 }
