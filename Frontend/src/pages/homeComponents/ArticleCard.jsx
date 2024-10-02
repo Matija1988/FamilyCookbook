b5ab4f96f1c6dbb7e5248bd71675c0ec8daa3795
Matija1988 <matijapavkovic74@gmail.com>
2024-10-02 11:59:22 +0200

    Rating display in article card

--- Frontend/src/pages/homeComponents/ArticleCard.jsx
+++ Frontend/src/pages/homeComponents/ArticleCard.jsx
@@ -10,14 +10,54 @@ function ArticleCard({ recipe }) {
 
   const navigate = useNavigate();
 
+  const renderStars = (rating) => {
+    const stars = [];
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating % 1 >= 0.5;
+    const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
+
+    for (let i = 0; i < fullStars; i++) {
+      stars.push(
+        <span key={`full-${i}`} style={{ color: "#FFD700" }}>
+          ★
+        </span>
+      );
+    }
+
+    if (hasHalfStar) {
+      stars.push(
+        <span key={`half`} style={{ color: "#FFD700" }}>
+          ☆
+        </span>
+      );
+    }
+
+    for (let i = 0; i < emptyStars; i++) {
+      stars.push(
+        <span key={`empty-${i}`} style={{ color: "#CCCCCC" }}>
+          ☆
+        </span>
+      );
+    }
+
+    // for (let i = 0; i < 5; i++) {
+    //   stars.push(
+    //     <span key={i} style={{ color: i < rating ? "#FFD700" : "#CCCCCC" }}>
+    //       ★
+    //     </span>
+    //   );
+    // }
+    return stars;
+  };
+
   return (
-    
     <Card key={recipe.id} className="article-card">
       <Card.Img variant="top" src={URL + recipe.pictureLocation} />
       <Card.Body>
         <Card.Title>{recipe.title}</Card.Title>
         <Card.Text className="card-subtitle">{recipe.subtitle}</Card.Text>
         <Card.Text className="card-category">{recipe.categoryName}</Card.Text>
+        <div>{renderStars(recipe.averageRating)}</div>
         <div className="card-button">
           <Button
             variant="primary"
