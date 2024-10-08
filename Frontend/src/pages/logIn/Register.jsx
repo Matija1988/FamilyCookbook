efd05d1ad08279d461dfe8fe5e70b1f5ab5707bf
Matija1988 <matijapavkovic74@gmail.com>
2024-10-08 20:32:57 +0200

    Frontend registration form

--- Frontend/src/pages/logIn/Register.jsx
+++ Frontend/src/pages/logIn/Register.jsx
@@ -1,47 +1,73 @@
 import { Col, Container, Form, Row } from "react-bootstrap";
-import InputText    from "../../components/InputText";
+import InputText from "../../components/InputText";
 import DateAndTime from "../../components/DateAndTime";
 import CustomButton from "../../components/CustomButton";
 import { useNavigate } from "react-router-dom";
 import { RouteNames } from "../../constants/constants";
+import RegisterService from "../../services/RegisterService";
+import useError from "../../hooks/useError";
+import ErrorModal from "../../components/ErrorModal";
 
 export default function Register() {
+  const initialState = {
+    firstName: "",
+    lastName: "",
+    dateOfBirth: {},
+    username: "",
+    password: "",
+  };
   const navigate = useNavigate();
-  async function handleSubmit() {}
 
-  const handleFirstNameChange = () => {};
+  const { showError, showErrorModal, hideError, errors } = useError();
 
-  const handleLastNameChange = () => {};
+  async function registerUser(e) {
+    const response = await RegisterService.RegisterUser(e);
+    if (!response.ok) {
+      showError(response.data);
+      return;
+    }
+    navigate(RouteNames.HOME);
+  }
 
-  const handleUsernameChange = () => {};
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-  const handlePasswordChange = () => {};
+    const form = e.target;
+    const information = new FormData(form);
+
+    if (information.get("dateOfBirth") != "") {
+      initialState.dateOfBirth =
+        information.get("dateOfBirth") + "T00:00:00.000Z";
+    }
+
+    initialState.firstName = information.get("First name");
+    initialState.lastName = information.get("Last name");
+    initialState.username = information.get("Username");
+    initialState.password = information.get("Password");
+
+    registerUser({
+      firstName: initialState.firstName,
+      lastName: initialState.lastName,
+      username: initialState.username,
+      password: initialState.password,
+      dateOfBirth: initialState.dateOfBirth,
+    });
+  }
+
   return (
     <>
       <Container className="w-25">
         <Form onSubmit={handleSubmit}>
           <Form.Label>REGISTER</Form.Label>
-          <InputText
-            atribute="First name"
+          <InputText atribute="First name" required={true}></InputText>
+          <InputText atribute="Last name" required={true}></InputText>
+          <InputText atribute="Username" required={true}></InputText>
+          <InputText atribute="Password" required={true}></InputText>
+          <DateAndTime
+            atribute="Date of birth"
             required={true}
-            onChange={handleFirstNameChange}
-          ></InputText>
-          <InputText
-            atribute="Last name"
-            required={true}
-            onChange={handleLastNameChange}
-          ></InputText>
-          <InputText
-            atribute="Username"
-            required={true}
-            onChange={handleUsernameChange}
-          ></InputText>
-          <InputText
-            atribute="Password"
-            required={true}
-            onChange={handlePasswordChange}
-          ></InputText>
-          <DateAndTime atribute="Date of birth" required={true}></DateAndTime>
+            propertyName="dateOfBirth"
+          ></DateAndTime>
           <Row>
             <Col>
               <CustomButton
@@ -61,6 +87,11 @@ export default function Register() {
           </Row>
         </Form>
       </Container>
+      <ErrorModal
+        show={showErrorModal}
+        onHide={hideError}
+        errors={errors}
+      ></ErrorModal>
     </>
   );
 }
---
+++ Frontend/src/services/RegisterService.js
@@ -0,0 +1,14 @@
+import { handleSuccess, httpService, processError } from "./HttpService";
+
+async function RegisterUser(user) {
+  return await httpService
+    .post("Register/registerUser", user)
+    .then((res) => {
+      return handleSuccess(res);
+    })
+    .catch((e) => {
+      return processError(e);
+    });
+}
+
+export default { RegisterUser };
